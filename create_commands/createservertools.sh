5260d6c06ccd4735b5172ff491acd5d9ec8e00ea
Leon V <33411715+leonv024@users.noreply.github.com>
2018-03-19 15:37:35 +0100

    Done (requires lynx for Webmin)
    Checks packages and asks to install them if missing.

--- create_commands/createservertools.sh
+++ create_commands/createservertools.sh
@@ -1,1 +1,60 @@
 #!/bin/bash
+
+function checkroot {
+  if [ $(id -u) == 0 ]; then
+    checkpackages
+  else
+    echo -e "\e[1;31mPlease, use sudo\e[0m"
+  fi
+}
+
+function webmin {
+  # Grab download link
+  _link=$(lynx --dump --listonly http://www.webmin.com/download.html \
+        | grep http | cut -f2- -d'.' | tr -d ' ' | sort | uniq | grep .deb | sed ${1}q;)
+  _file=$(echo "$_link" | cut -d '/' -f5)
+  echo "[Downloading] Grabbing $_link"
+
+  wget $_link -P /tmp/ -q
+  echo "[Installing] Installing $_file from /tmp/"
+  dpkg -i /tmp/$_file; apt install -f
+
+  # Remove downloaded file
+  rm /tmp/$_file
+}
+
+function checkpackages {
+  # Packages to check
+  _packages=("lynx" "webmin" "openssh-server" "openssh-client" "mc" "ntopng" "isc-dhcp-server" \
+        "samba" "lxd" "lxd-client" "quota")
+
+  # For item in list, check if installed
+  #   if not >> ask to install
+  for item in ${_packages[*]}
+  do
+    if [ $(dpkg-query -W -f='${Status}' $item 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
+      echo -e "\e[1;31m[ MISSING ]\e[0m $item"
+        printf "Do you wish to install this now? (y/n) "; read _yn
+      if [[ $_yn =~ ^[Yy]$ ]]; then
+        echo -e "\e[1;33m[ INSTALLING ] $item"
+
+        # For webmin, be special
+        if [ $item == "webmin" ]; then
+          webmin
+
+        else
+          # Install package
+          sudo apt-get install $item
+        fi
+        # Done installing package
+        echo -e "\e[1;33m[ DONE ] $item\e[0m"
+      fi
+    else
+      # Already installed
+      echo -e "\e[1;32m[OK]\e[0m $item"
+    fi
+  done
+
+}
+
+checkroot
