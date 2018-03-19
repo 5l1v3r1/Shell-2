c0a919d6cd8ec8c51638e1983a0c9c2cacb66898
Leon V <33411715+leonv024@users.noreply.github.com>
2018-03-19 17:31:13 +0100

    Fixed webmin installation

--- create_commands/createservertools.sh
+++ create_commands/createservertools.sh
@@ -12,12 +12,17 @@ function webmin {
   # Grab download link
   _link=$(lynx --dump --listonly http://www.webmin.com/download.html \
         | grep http | cut -f2- -d'.' | tr -d ' ' | sort | uniq | grep .deb | sed ${1}q;)
-  _file=$(echo "$_link" | cut -d '/' -f5)
-  echo "[Downloading] Grabbing $_link"
 
+  # Grab filename
+  _file=$(echo "$_link" | cut -d '/' -f5)
+
+  echo -e "\e[32mDownloading\e[0m $_link"
   wget $_link -P /tmp/ -q
-  echo "[Installing] Installing $_file from /tmp/"
-  dpkg -i /tmp/$_file; apt install -f
+
+  echo -e "\e[32mInstalling\e[0m /tmp/$_file"
+  apt-get install apt-show-versions libauthen-pam-perl
+
+  dpkg -i /tmp/$_file
 
   # Remove downloaded file
   rm /tmp/$_file
@@ -33,10 +38,10 @@ function checkpackages {
   for item in ${_packages[*]}
   do
     if [ $(dpkg-query -W -f='${Status}' $item 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
-      echo -e "\e[1;31m[ MISSING ]\e[0m $item"
-        printf "Do you wish to install this now? (y/n) "; read _yn
+      echo -e "\e[31m[ MISSING ]\e[0m $item"
+        printf "Do you wish to install this now? (y/N) "; read _yn
       if [[ $_yn =~ ^[Yy]$ ]]; then
-        echo -e "\e[1;33m[ INSTALLING ] $item"
+        echo -e "\e[32mInstalling\e[0m $item"
 
         # For webmin, be special
         if [ $item == "webmin" ]; then
@@ -44,14 +49,14 @@ function checkpackages {
 
         else
           # Install package
-          sudo apt-get install $item
+          apt-get install $item -y
         fi
         # Done installing package
-        echo -e "\e[1;33m[ DONE ] $item\e[0m"
+        echo -e "\e[32mDone\e[0m $item"
       fi
     else
       # Already installed
-      echo -e "\e[1;32m[OK]\e[0m $item"
+      echo -e "\e[32m[OK]\e[0m $item"
     fi
   done
 
