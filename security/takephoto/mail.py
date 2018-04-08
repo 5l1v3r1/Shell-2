4d89d08c372c1b6a1a2f16711f0486b4759d2bda
Leon V <33411715+leonv024@users.noreply.github.com>
2018-04-08 16:52:09 +0200

    Added the location

--- security/takephoto/mail.py
+++ security/takephoto/mail.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 
-import os, smtplib, getpass, sys, time, socket
-from requests import get
+import os, smtplib, getpass, sys, time, socket, requests
 
 def getlocalip():
     # Connect to a reomte server and return the socketname, aka the ip address
@@ -13,24 +12,25 @@ def getlocalip():
 def getexternalip():
     # This grabs the content of a remote website.
     # Usung 'get' from requests
-    ip = get('https://ident.me').text
+    ip = requests.get('https://ident.me').text
     return ip
 
-# --------------------------------------------------------
-# I recommand using a dummy email for this.
-# This account must not have two-step-verification
-# Paste the password in clear-text
+def location(ip):
+    location = requests.get('https://freegeoip.net/json/' + '%s' % ip)
+    result = location.json()
 
-to = '<ENTER EMAIL ADDRESS>'    # To Email
-user = '<ENTER EMAIL ADDRESS>'  # Sent with this Email account
-passwd = '<YOUR PASSWORD>'      # Email Password
+    data = []
+    data.append(result["country_name"])
+    return data
 
-smtp = 'smtp.gmail.com'         # SMTP server
-smtp_port = 587                 # SMTP port
-# --------------------------------------------------------
+to = 'lh.voerman@gmail.com'
+user = 'lv-laptop@leonvoerman.nl'
+passwd = '67cnhFdz'
 
 body = '''
 Dear user,
+
+Incorrect password attempt detected...
 
    _______     _______ _______ ______ __  __
   / ____\ \   / / ____|__   __|  ____|  \/  |
@@ -54,25 +54,27 @@ Local
  [%s]
 
 External
- [%s]
+ [%s] -> [%s]
 
 Kind regards,
 
 Administrator
+Leon Voerman
 
-''' % (time.strftime('%d-%m-%Y %X'), socket.gethostbyaddr('127.0.1.1')[0], getlocalip(), getexternalip())
+''' % (time.strftime('%d-%m-%Y %X'), socket.gethostbyaddr('127.0.1.1')[0], getlocalip(), getexternalip(), location(getexternalip())[0])
 
 print ''
 
 try:
-    server = smtplib.SMTP(smtp,int(smtp_port))
+    server = smtplib.SMTP('smtp02.hostnet.nl','587')
     server.ehlo()
     server.starttls()
     server.login(user,passwd)
-    subject = 'Incorrect password attempt'
-    msg = 'From: ' + user + '\nSubject: ' + subject + '\n' + body
-    server.sendmail(user,to,msg) # Send message
-    server.quit()
+    for i in range(1, 1+1): # Send one
+       subject = 'Incorrect password attempt on lv-laptop'
+       msg = 'From: ' + user + '\nSubject: ' + subject + '\n' + body
+       server.sendmail(user,to,msg) # Send message
+       server.quit()
 
        print '\nMessage was successfully sent to: ' + to
 
